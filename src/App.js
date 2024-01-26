92aa772d8bbf7afbe1484d7c41a9ebbd8797b98f
Pedro Xavier <pedroxxavier@hotmail.com>
2024-01-26 18:11:13 -0300

    Passando o projeto para o React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import "./App.css";
-import Header from "./Header/Header";
+import Header from "../src/componentes/Header/Header";
+import Sidebar from "./componentes/Sidebar/Sidebar";
+import Main from "./componentes/Main/Main";
+import Footer from "./componentes/Footer/Footer";
 
 function App() {
-  return <Header></Header>;
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
